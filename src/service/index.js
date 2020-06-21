17835d3bcbf1d1a634bc7401b7f96e03b3477191
chenjiong <chen_jio@163.com>
2020-06-21 14:58:02 +0800

    封装axios

---
+++ src/service/index.js
@@ -0,0 +1,12 @@
+import http from './axios'
+
+export const login = (params) => {
+  return http
+    .post('/login', {
+      username: '123',
+      password: 123
+    })
+    .then((res) => {
+      console.log(res)
+    })
+}
--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import './style.less'
-
+import { login } from '../../service'
 
 class Login extends Component {
+  login = () => {
+    login()
+  }
   render() {
     return (
       <div>
-        <span className='Login__text--color'>login</span>
+        <span className="Login__text--color" onClick={this.login}>
+          login
+        </span>
       </div>
     )
   }
